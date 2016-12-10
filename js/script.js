0b93ccac35be0f06f47aa092b2c8102d088a5143
shate <shate@ya.ru>
2016-12-10 12:44:13 +0300

    Вариант1

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
 $(document).ready(function(){
-	
+
+            $('.header_nav a').click(function() {
+                var scroll_el = $(this).attr('href');
+                if ($(scroll_el).length != 0) {
+                    $('html, body').animate({ scrollTop: $(scroll_el).offset().top }, 500);                 }
+                return false;
+            });
+
+
+
 })
